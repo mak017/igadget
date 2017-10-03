d124b0ca8477bc9510bcb94b5e1485ca114ee3db
Denis Makovskyy <makovskyy0983@gmail.com>
2017-10-04 00:13:19 +0300

    Removed GA scripts from luna and survey pages

--- luna/index.php
+++ luna/index.php
@@ -21,21 +21,6 @@ $price_old=floor(($price_new/(100-$skidka))*100);
 <html lang="ru">
 
 <head>
-		<!-- Google Analytics Content Experiment code -->
-		<script>function utmx_section(){}function utmx(){}(function(){var
-		k='139486531-1',d=document,l=d.location,c=d.cookie;
-		if(l.search.indexOf('utm_expid='+k)>0)return;
-		function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
-		indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
-		length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
-		'<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
-		'://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
-		'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
-		valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-		'" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();
-		</script><script>utmx('url','A/B');</script>
-		<!-- End of Google Analytics Content Experiment code -->
-		
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
